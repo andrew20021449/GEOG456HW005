417c67ed0586fe19de7876262401e64bed05abdf
andrew20021449 <andrewbryant2002@gmail.com>
2022-09-16 17:55:05 -0400

    lots of stuff

--- BARTLines.js
+++ BARTLines.js
@@ -1,4 +1,4 @@
-{
+var BARTLines = {
 "type": "FeatureCollection",
 "name": "BARTLines",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
--- BARTStations.js
+++ BARTStations.js
@@ -1,4 +1,4 @@
-{
+var BARTStations = {
 "type": "FeatureCollection",
 "name": "BARTStations",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
